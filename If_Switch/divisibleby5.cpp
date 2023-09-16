02a3721f6b0e42d366ec13900967ee17137c3147
princekr1603 <princekr1603@gmail.com>
2023-09-17 01:36:32 +0530

    updated

---
+++ If_Switch/divisibleby5.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"enter a no.:";
+    int n;
+    cin>>n;
+    if(n%5==0){
+        cout<<n<<" is divisible by 5";
+    }
+    else{
+        cout<<n<<" is not divisible by 5";
+    }
+
+
+}
---
+++ If_Switch/weakbyswitch.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"enter day no.";
+    cin>>x;
+    switch(x){
+        case 1 :
+            cout<<"monday";
+            break;
+        case 2 :
+            cout<<"tuesday";
+            break;
+        case 3 :
+            cout<<"wednesday";
+            break;
+        case 4 :
+            cout<<"thursday";
+            break;
+        case 5 :
+            cout<<"friday";
+            break;
+        case 6 :
+            cout<<"saturday";
+            break;
+        case 7 :
+            cout<<"sunday";
+            break;
+
+    }
+}

c66dcb629e9eb89f956dd40053b1814263d91583
princekr1603 <princekr1603@gmail.com>
2023-09-19 22:49:24 +0530

    updated

---
+++ LOOPS/continue.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x=4,y=0,z;
+    while(x>=0){
+        x--;
+        y++;
+        if(x==y) continue;
+        else cout<<x<<" "<<y<<endl;
+    }
+
+}

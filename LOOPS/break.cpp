f1b11a46bd3adcf54aaf03e24555683dde78c202
princekr1603 <princekr1603@gmail.com>
2023-09-19 22:54:17 +0530

    update

---
+++ LOOPS/break.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x=4,y=0;
+    while(x>=0){
+        if(x==y) break;
+        else 
+        cout<<x<<" "<<y<<endl;
+        x--;
+        y++;
+
+    }
+}

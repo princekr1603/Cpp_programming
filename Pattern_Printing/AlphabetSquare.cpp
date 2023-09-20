69aa8b17437d2b47db072d32518d6f60dc4b7370
princekr1603 <princekr1603@gmail.com>
2023-09-21 00:10:40 +0530

    update

---
+++ Pattern_Printing/AlphabetSquare.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter no.:";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cout<<char(64+j);
+        }
+        cout<<endl;
+    }
+}

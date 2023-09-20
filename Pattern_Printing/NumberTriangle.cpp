7d8027d1f9358526c8e1822d99fec63349af4a79
princekr1603 <princekr1603@gmail.com>
2023-09-20 23:51:18 +0530

    update

--- Pattern_Printing/NumberTriangle.cpp
+++ Pattern_Printing/NumberTriangle.cpp
@@ -5,8 +5,8 @@ int main(){
     cout<<"enter no.:";
     cin>>n;
     for(int i=1;i<=n;i++){
-        for(int j=n;j>=i;j--){//for(int j=1;j<=n+1-i;j++)
-            cout<<"* ";
+        for(int j=1;j<=i;j++){
+            cout<<j;
         }
         cout<<endl;
     }

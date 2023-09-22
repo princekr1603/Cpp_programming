15d0fbee620b40bd4b4c920eec9dd8f01ff2b948
princekr1603 <princekr1603@gmail.com>
2023-09-22 21:37:21 +0530

    update

--- Pattern_Printing/StarTriangleFlipped.cpp
+++ Pattern_Printing/StarTriangleFlipped.cpp
@@ -4,12 +4,26 @@ int main(){
     int n;
     cout<<"enter no.:";
     cin>>n;
+
+    //[m-1]
+
+    // for(int i=1;i<=n;i++){          
+    //     for(int j=1;j<=n-i;j++){
+    //         cout<<" "; 
+    //     }
+    //     for(int k=1;k<=i;k++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+    //[m-2]
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=n-i;j++){
-            cout<<" "; 
-        }
-        for(int k=1;k<=i;k++){
-            cout<<"*";
+        for(int j=1;j<=n;j++){
+            if(i+j>=n+1){
+                cout<<"*";
+            }
+            else cout<<" ";
         }
         cout<<endl;
     }

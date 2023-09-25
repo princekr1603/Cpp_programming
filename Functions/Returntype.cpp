d4516c3ec932deff8b7534f1fde3045ba96f63f7
princekr1603 <princekr1603@gmail.com>
2023-09-26 01:54:52 +0530

    update

--- Functions/Returntype.cpp
+++ Functions/Returntype.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 int sum(int x,int y){
     return x+y;
 }
 int main(){
     // cout<<sum(29,68);
-    int x,y;
-    cin>>x>>y;
-    cout<<min(x,y);
+    // int x,y;
+    // cin>>x>>y;
+    // cout<<max(x,y)
+    cout<<sqrt(7);
 }

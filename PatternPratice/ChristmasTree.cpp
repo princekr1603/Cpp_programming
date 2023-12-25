f415dd5a04c71e63f513efa9c35710d9843bf541
princekr1603 <princekr1603@gmail.com>
2023-12-25 19:21:37 +0530

    update

--- PatternPratice/ChristmasTree.cpp
+++ PatternPratice/ChristmasTree.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
+#include<windows.h>
 using namespace std;
 int main(){
+    HANDLE h= GetStdHandle(STD_OUTPUT_HANDLE);
     int n;
     cout<<"enter no.:";
     cin>>n;
@@ -10,6 +12,7 @@ int main(){
             cout<<" ";
         }
         for(int k=1;k<=2*i-1;k++){
+            SetConsoleTextAttribute(h,2);
             cout<<"*";
         }
         cout<<endl;
@@ -18,6 +21,7 @@ int main(){
         for(int j=1;j<=n;j++){
             int a=(2*n-1)/2;
             if(j==a){
+                SetConsoleTextAttribute(h,4);
                 cout<<"***";
             }
             else{

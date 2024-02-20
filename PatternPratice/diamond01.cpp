19693dd28588e5ab9ccce7130a98890b6c2aa70f
Prince <princekr1603@gmail.com>
2024-02-20 21:15:46 +0530

    update

--- PatternPratice/diamond01.cpp
+++ PatternPratice/diamond01.cpp
@@ -11,18 +11,18 @@ int main(){
             }
             else{
                 cout<<"1";
-            }
-            
+            }   
         }
         cout<<endl;
     }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<(n+2)-2*i;j++){
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=2*(n-i)-1;j++){
              if(j%2==0){
-                cout<<"1";
+                cout<<"0";
             }
             else{
-                cout<<"0";
+                cout<<"1";
             }
         }
         cout<<endl;

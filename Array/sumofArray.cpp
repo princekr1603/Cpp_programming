53445fdba3442627e9ba09664f7ddf263024220a
Prince <princekr1603@gmail.com>
2024-09-01 19:06:58 +0530

    update

--- Array/sumofArray.cpp
+++ Array/sumofArray.cpp
@@ -1,13 +1,16 @@
 #include<iostream>
 using namespace std;
 int main( ){
-    int arr[5];
-    for(int i=1;i<=5;i++){
+    int n;
+    cout<<"enter the size of array: ";
+    cin>>n;
+    int arr[n];
+    for(int i=1;i<=n;i++){
         cout<<"enter value of array: ";
         cin>>arr[i];
     }
     int r=0;
-    for(int i=1;i<=5;i++){
+    for(int i=1;i<=n;i++){
         r=r+arr[i];
     }
     cout<<r;

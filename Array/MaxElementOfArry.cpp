8336ff42ed386f4dd31500b14fdaad3ef96eaa27
Prince <princekr1603@gmail.com>
2025-02-12 19:32:22 +0530

    update

--- Array/MaxElementOfArry.cpp
+++ Array/MaxElementOfArry.cpp
@@ -1,5 +1,22 @@
 #include<iostream>
+//#include<climits>
 using namespace std;
 int main(){
-    cout<<"hello prince";
+    int n;
+    cout<<"enter size: ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cout<<"enter values: ";
+        cin>>arr[i];
+    }
+    int max=arr[0];
+    //int max=INT_MIN;
+    for(int i=1;i<n;i++){
+        if(max<arr[i]){
+            max=arr[i];
+        }
+    }
+    cout<<"maximum is: "<<max;
+    
 }
